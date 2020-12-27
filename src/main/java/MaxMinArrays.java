0debb55a881be927f76db83fa7c4c5535dbcae05
roman <aboandrus@gmail.com>
2020-12-27 15:55:20 +0100

    Implemented code

--- src/main/java/MaxMinArrays.java
+++ src/main/java/MaxMinArrays.java
@@ -1,9 +1,31 @@
+import java.util.Arrays;
+
 public class MaxMinArrays {
 
     public static int[] solve (int[] arr){
+        Arrays.sort(arr);
+        int[] resultArray = new int[arr.length];
 
+        int indexResult =0;
+        int indexStart = 0;
+        int indexEnd = arr.length -1;
 
-        //..
+        while(indexStart<=indexEnd){
+
+            if(indexResult<arr.length){
+                resultArray[indexResult]=arr[indexEnd];
+            }
+
+            indexResult++;
+            if(indexResult<arr.length){
+                resultArray[indexResult]=arr[indexStart];
+            }
+
+            indexStart++;
+            indexEnd--;
+            indexResult++;
+        }
+
+        return  resultArray;
     }
-
 }
--- src/test/java/MaxMinArraysTest.java
+++ src/test/java/MaxMinArraysTest.java
@@ -7,8 +7,7 @@ public class MaxMinArraysTest {
     @Test
     public void basicTests(){
         assertArrayEquals(new int[]{15,7,12,10,11},MaxMinArrays.solve(new int[]{15,11,10,7,12}));
-        assertArrayEquals(new int[]{15,7,12,10,11},MaxMinArrays.solve(new int[]{15,11,10,7,12}));
-        assertArrayEquals(new int[]{15,7,12,10,11},MaxMinArrays.solve(new int[]{15,11,10,7,12}));
+
     }
 
 
